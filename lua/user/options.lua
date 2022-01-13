e2af94b420e46845a84b1797b6485ba05b209054
Lucas <lucasalvarez@pm.me>
2022-01-12 21:04:55 -0500

    Add keymaps

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,8 +1,26 @@
+-- I Don't even know
+local opts = { noremap = true, silent = true }
+
+-- Shorten Repeated Commands
+local km = vim.api.nvim_set_keymap
+local vo = vim.opt
+local vc = vim.cmd
+
 -- Line Number Options
-vim.opt.number = true
-vim.opt.relativenumber = true
+vo.number = true
+vo.relativenumber = true
 
 -- Keep Selection After Indent
-vim.cmd ":vnoremap < <gv"
-vim.cmd ":vnoremap > >gv"
+vc ":vnoremap < <gv"
+vc ":vnoremap > >gv"
 
+-- Remap space as leader key
+km("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Move text up and down
+km("x", "J", ":move '>+1<CR>gv-gv", opts)
+km("x", "K", ":move '<-2<CR>gv-gv", opts)
+km("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+km("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
